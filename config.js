b0ad00038b53d6c37e5b495cecd2b4b4bfb1403f
SATHAN <sathanic498@gmail.com>
2025-08-31 04:05:40 +0530

    Z E R A  X

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botName: "ZERA X",            // Bot name
+    ownerName: "Sathan",       // Owner name (edit later)
+    ownerNumber: "919778158839",  // Owner number (edit later)
+    prefix: ".",                  // Bot command prefix
+    sessionFolder: "session",     // Session folder
+    databaseType: "sqlite"        // or "mongodb"
+};
